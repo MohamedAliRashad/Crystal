5229a33de0900cec0e9789b50b02c176b774569b
MohamedAliRashad <m.rashadnow@gmail.com>
2020-03-31 03:48:06 +0200

    Add download route

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+@app.route("/home")
+def Home():
     return render_template("home.html")
 
+@app.route("/download")
+def Download():
+    return render_template("download.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
