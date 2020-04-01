f97825671eeba0067807d53bdd2988e4665998c6
AkramElganiny <akram.elganiny@gmail.com>
2020-04-02 00:53:41 +0200

    initial website

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'mp4'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = 'super secret'
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=['GET', 'POST'])
 def Home():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        print(file)
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # print(url_for('uploaded_file', filename=filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     return render_template("home.html")
 
 @app.route("/download")
 def Download():
     return render_template("download.html")
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    app.run()
---
+++ static/js/custom.js
@@ -0,0 +1,5 @@
+<!-- for desktop -->
+<script> window.disableOnScroll = 1; </script>
+
+                <!-- for mobile -->
+<script> window.disableOnSwipe = 1; </script>
