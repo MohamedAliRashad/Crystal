3ed06f74f1b0a3b9e54ec8a92eb0844bbb7cc1e2
AkramElganiny <akram.elganiny@gmail.com>
2020-04-24 08:17:15 +0200

    uploading progress feature

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory, render_template, request, make_response, jsonify
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './uploads'
@@ -10,40 +10,43 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = 'super secret'
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route("/", methods=['GET', 'POST'])
 def Home():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
+    if request.method == "POST":
+
+        file = request.files["file"]
+
+        print("File uploaded")
         print(file)
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # print(url_for('uploaded_file', filename=filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            res = make_response(jsonify({"message": "File uploaded"}), 200)
+            return res
+        else:
+            res = make_response(jsonify({"message": "Invalid Video"}), 400)
+            return res
+
     return render_template("home.html")
+
 
 @app.route("/download")
 def Download():
     return render_template("download.html")
 
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
 
 if __name__ == "__main__":
     app.debug = True
