6fb6ccc9c89f72749674a85ef8f3c70a2929c7c4
Abdelrahman-Bishr <abdelrahman.bishr@gmail.com>
2020-05-12 00:14:42 +0200

    initial class structuring

--- SR-EDVR/codes/utils/video_utils.py
+++ SR-EDVR/codes/utils/video_utils.py
@@ -87,3 +87,56 @@ def remove_subfolders():
 def moveProcessedFrames():
   shutil.rmtree(osp.join(parent_dir,'video/inframes'))
   os.rename(osp.join(parent_dir,'video/outframes'), osp.join(parent_dir,'video/inframes'))
+
+
+
+def build_video(source_path: Path) -> Path:
+        out_path = result_folder / (
+            source_path.name.replace('.mp4', '_no_audio.mp4')
+        )
+        outframes_folder = outframes_root / (source_path.stem)
+        outframes_path_template = str(outframes_folder / '%5d.jpg')
+        out_path.parent.mkdir(parents=True, exist_ok=True)
+        if out_path.exists():
+            out_path.unlink()
+        fps = get_fps(source_path)
+        print('Original FPS is: ', fps)
+
+        ffmpeg.input(
+            str(outframes_path_template),
+            format='image2',
+            vcodec='mjpeg',
+            framerate=fps,
+        ).output(str(out_path), crf=17, vcodec='libx264').run(capture_stdout=True)
+
+        result_path = result_folder / source_path.name
+        if result_path.exists():
+            result_path.unlink()
+        # making copy of non-audio version in case adding back audio doesn't apply or fails.
+        shutil.copyfile(str(out_path), str(result_path))
+
+        # adding back sound here
+        audio_file = Path(str(source_path).replace('.mp4', '.aac'))
+        if audio_file.exists():
+            audio_file.unlink()
+
+        os.system(
+            'ffmpeg -y -i "'
+            + str(source_path)
+            + '" -vn -acodec copy "'
+            + str(audio_file)
+            + '"'
+        )
+
+        if audio_file.exists:
+            os.system(
+                'ffmpeg -y -i "'
+                + str(out_path)
+                + '" -i "'
+                + str(audio_file)
+                + '" -shortest -c:v copy -c:a aac -b:a 256k "'
+                + str(result_path)
+                + '"'
+            )
+        print('Video created here: ' + str(result_path))
+        return result_path
