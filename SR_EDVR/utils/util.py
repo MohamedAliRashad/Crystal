02a79e8285b67a27ce72b09d0ef095706f637280
MohamedAliRashad <m.rashadnow@gmail.com>
2020-05-19 14:28:58 +0200

    Fix make_grid bug

--- SR_EDVR/utils/util.py
+++ SR_EDVR/utils/util.py
@@ -10,7 +10,6 @@ from collections import OrderedDict
 import numpy as np
 import cv2
 import torch
-from torchvision.utils import make_grid
 from shutil import get_terminal_size
 
 import yaml
@@ -112,17 +111,13 @@ def crop_border(img_list, crop_border):
 def tensor2img(tensor, out_type=np.uint8, min_max=(0, 1)):
     '''
     Converts a torch Tensor into an image Numpy array
-    Input: 4D(B,(3/1),H,W), 3D(C,H,W), or 2D(H,W), any range, RGB channel order
+    Input: 3D(C,H,W), or 2D(H,W), any range, RGB channel order
     Output: 3D(H,W,C) or 2D(H,W), [0,255], np.uint8 (default)
     '''
     tensor = tensor.squeeze().float().cpu().clamp_(*min_max)  # clamp
     tensor = (tensor - min_max[0]) / (min_max[1] - min_max[0])  # to range [0,1]
     n_dim = tensor.dim()
-    if n_dim == 4:
-        n_img = len(tensor)
-        img_np = make_grid(tensor, nrow=int(math.sqrt(n_img)), normalize=False).numpy()
-        img_np = np.transpose(img_np[[2, 1, 0], :, :], (1, 2, 0))  # HWC, BGR
-    elif n_dim == 3:
+    if n_dim == 3:
         img_np = tensor.numpy()
         img_np = np.transpose(img_np[[2, 1, 0], :, :], (1, 2, 0))  # HWC, BGR
     elif n_dim == 2:
